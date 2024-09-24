308df7d76a950caabedf451722d1d87ef79e5bf6
limbaba1120 <kwlim1120ss@gmail.com>
2024-09-24 18:00:19 +0900

    feat: 경매 게시글 CRUD 구현 - member, auction, image entity 설계 - 경매 (create, get, delete) 구현 - S3에 이미지 저장

--- omocha-client/src/main/java/org/auction/client/auction/interfaces/request/CreateAuctionRequest.java
+++ omocha-client/src/main/java/org/auction/client/auction/interfaces/request/CreateAuctionRequest.java
@@ -6,7 +6,6 @@ import org.auction.domain.auction.domain.enums.AuctionType;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -14,8 +13,8 @@ public record CreateAuctionRequest(
 	@NotBlank String title,
 	String content,
 	// REFACTOR : 나중에 회의 후 VO를 도입할지 어떻게 처리할지 알아봐야함
-	@NotNull @Min(1) Integer startPrice,
-	@NotNull @Min(1) Integer bidUnit,
+	@NotNull Integer startPrice,
+	@NotNull Integer bidUnit,
 	@NotNull AuctionType auctionType,
 	@NotNull @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	LocalDateTime startDate,
