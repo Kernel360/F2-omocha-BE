fe9ccefcee7bae505773825294adc1be014fef19
limbaba1120 <kwlim1120ss@gmail.com>
2024-09-26 16:07:04 +0900

    deploy: swagger http -> https 변경

--- omocha-client/src/main/java/org/auction/client/config/swagger/SwaggerConfig.java
+++ omocha-client/src/main/java/org/auction/client/config/swagger/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package org.auction.client.config.swagger;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,8 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.swagger.v3.core.jackson.ModelResolver;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -35,5 +38,15 @@ public class SwaggerConfig {
 			.group("사용자를 위한 API")
 			.pathsToMatch(paths)
 			.build();
+	}
+
+	// 로컬 환경 및 프로덕션 환경 모두에서 테스트할 수 있도록 OpenAPI 서버 설정
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+			.servers(List.of(
+				new Server().url("https://api.omocha-auction.com").description("Production server"),
+				new Server().url("http://localhost:8080").description("Local server")  // 로컬 테스트 서버 추가
+			));
 	}
 }
