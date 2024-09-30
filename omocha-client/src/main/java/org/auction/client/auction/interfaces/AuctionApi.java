e586a28a7941e51efeec8e8c6c3e3a28e7ad387b
limbaba1120 <kwlim1120ss@gmail.com>
2024-09-30 12:26:54 +0900

    fix: AuctionApi 코드 수정

--- omocha-client/src/main/java/org/auction/client/auction/interfaces/AuctionApi.java
+++ omocha-client/src/main/java/org/auction/client/auction/interfaces/AuctionApi.java
@@ -7,7 +7,11 @@ import org.auction.client.auction.interfaces.request.CreateAuctionRequest;
 import org.auction.client.auction.interfaces.response.AuctionDetailResponse;
 import org.auction.client.auction.interfaces.response.CreateAuctionResponse;
 import org.auction.client.common.dto.ResultDto;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 import io.swagger.v3.oas.annotations.Operation;
