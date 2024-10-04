cdb7bf31d04f0dd28e2624c747ef5b8053156c81
limbaba1120 <kwlim1120ss@gmail.com>
2024-10-04 12:31:30 +0900

    fix : security log 수정

--- omocha-client/src/main/java/org/auction/client/config/security/SecurityConfig.java
+++ omocha-client/src/main/java/org/auction/client/config/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package org.auction.client.config.security;
 import org.auction.client.config.security.filter.JwtAuthFilter;
 import org.auction.client.config.security.handler.CustomAccessDeniedHandler;
 import org.auction.client.config.security.handler.CustomAuthenticationEntryPointHandler;
-import org.auction.domain.member.domain.enums.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -29,7 +28,9 @@ public class SecurityConfig {
 
 	//TODO: 정적 자원 접근에러 파악해야함
 	public static final String[] PERMITTED_ALL_URI = {
-		"/swagger-ui/**", "/v3/api-docs/*"
+		"/api/v1/auth/**",
+		"/swagger-ui/**",
+		"/v3/api-docs/**"
 	};
 
 	@Bean
@@ -44,10 +45,7 @@ public class SecurityConfig {
 			)
 			.authorizeHttpRequests(authorize -> authorize
 				.requestMatchers(PERMITTED_ALL_URI).permitAll()
-				.requestMatchers("/api/v1/auth/**").permitAll()
 				.requestMatchers(HttpMethod.GET, "/api/v1/auction/*").permitAll()
-				.requestMatchers(HttpMethod.GET, "/api/v1/auction").permitAll()
-				.requestMatchers(HttpMethod.POST, "/api/v1/auction").hasRole(Role.ROLE_USER.getRole())
 				.anyRequest().authenticated()
 			)
 
