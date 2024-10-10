ba63253468b90177f32bdafec839a12878930f6b
limbaba1120 <kwlim1120ss@gmail.com>
2024-10-10 17:20:16 +0900

    fix : local.omocha-auction.com

---
+++ omocha-client/src/main/java/org/auction/client/chat/interfaces/response/ChatRoomDetailsResponse.java
@@ -0,0 +1,20 @@
+package org.auction.client.chat.interfaces.response;
+
+import org.auction.client.common.dto.SliceResponse;
+import org.auction.domain.chat.domain.entity.ChatRoomEntity;
+import org.springframework.data.domain.Slice;
+
+public record ChatRoomDetailsResponse(
+	ChatRoomResponse chatRoomInfo, // 채팅방 정보
+	SliceResponse<ChatMessageResponse> messages // 메시지 목록
+) {
+	public static ChatRoomDetailsResponse toDto(
+		ChatRoomEntity chatRoomEntity,
+		Slice<ChatMessageResponse> messages
+	) {
+		return new ChatRoomDetailsResponse(
+			ChatRoomResponse.toDto(chatRoomEntity),
+			new SliceResponse<>(messages)
+		);
+	}
+}
