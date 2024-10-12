4dbd106a6ffdeb39b6ffd78e69f1bed25ad333cc
이재윤 <kjd99002@naver.com>
2024-10-12 19:43:18 +0900

    [REFACTOR] QnA parameter 수정 (#53)
    * refactor : QnA parameter 수정
    
    * refactor : SwaggerConfig 누락 추가

--- omocha-client/src/main/java/org/auction/client/qna/interfaces/response/CreateAnswerResponse.java
+++ omocha-client/src/main/java/org/auction/client/qna/interfaces/response/CreateAnswerResponse.java
@@ -1,11 +1,17 @@
 package org.auction.client.qna.interfaces.response;
 
+import java.time.LocalDateTime;
+
 import org.auction.domain.qna.domain.entity.AnswerEntity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public record CreateAnswerResponse(
 	Long questionId,
 	String title,
-	String content
+	String content,
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	LocalDateTime createAt
 ) {
 	public static CreateAnswerResponse toDto(
 		AnswerEntity answerEntity
@@ -13,7 +19,8 @@ public record CreateAnswerResponse(
 		return new CreateAnswerResponse(
 			answerEntity.getQuestionEntity().getQuestionId(),
 			answerEntity.getTitle(),
-			answerEntity.getContent()
+			answerEntity.getContent(),
+			answerEntity.getCreatedAt()
 		);
 
 	}
