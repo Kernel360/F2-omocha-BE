e6f3a93a82f48458b054e8b6ea303d83dbe2611f
이재윤 <kjd99002@naver.com>
2024-10-14 11:08:35 +0900

    [FEAT] mypage 기능 추가 (#60)
    * feat : mypage 기능 추가
    
    * feat : 사용자 정보 반환 코드 추가
    
    * feat : 사용자 경매 물품 내역 조회
    
    * feat : 사용자 입찰 내역 조회 추가
    
    * feat : mypage 관련 error, Api 추가
    
    * feat : PR 추가
    
    * refactor : MemberInfoResponse
    - Info 관련 필드 추가
    
    * refactor : Controller, Service 메소드 명 수정
    
    * refactor : MypageCode 상태 코드명 수정
    
    * refactor : 불 필요한 로직 삭제
    
    * feat : PR 누락 된 부분 추가
    
    * refactor : BidRepositoryImpl
    - query 수정
    
    * refactor : MypageController endpoint 수정
    
    * feat : PR 수정 부분 추가
    
    * refactor : AuctionRepositoryImpl 쿼리 수정
    
    * chore : MypageService TODO 추가

---
+++ omocha-client/src/main/java/org/auction/client/mypage/interfaces/MypageController.java
@@ -0,0 +1,144 @@
+package org.auction.client.mypage.interfaces;
+
+import org.auction.client.common.code.MypageCode;
+import org.auction.client.common.dto.ResultDto;
+import org.auction.client.jwt.UserPrincipal;
+import org.auction.client.mypage.application.MypageService;
+import org.auction.client.mypage.interfaces.response.MemberInfoResponse;
+import org.auction.client.mypage.interfaces.response.MypageAuctionListResponse;
+import org.auction.client.mypage.interfaces.response.MypageBidListResponse;
+import org.auction.domain.auction.domain.enums.AuctionStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/myinfo")
+public class MypageController implements MypageApi {
+
+	private final MypageService mypageService;
+
+	// TODO : 멤버 정보 반환? 고민해야됨
+	//		로그인시 or Api, + 회원 정보 추가
+	@GetMapping("/me")
+	public ResponseEntity<ResultDto<MemberInfoResponse>> getMe(
+		@AuthenticationPrincipal UserPrincipal userPrincipal
+	) {
+
+		log.info("getMe started");
+
+		Long memberId = userPrincipal.getId();
+
+		log.debug("get me getId {}", userPrincipal.getId());
+
+		MemberInfoResponse memberInfoResponse = mypageService.findMe(memberId);
+
+		ResultDto<MemberInfoResponse> resultDto = ResultDto.res(
+			MypageCode.MEMBER_INFO_RETRIEVE_SUCCESS.getStatusCode(),
+			MypageCode.MEMBER_INFO_RETRIEVE_SUCCESS.getResultMsg(),
+			memberInfoResponse
+		);
+
+		log.info("getMe finished");
+		log.debug("get me resultDto {}", resultDto);
+
+		return ResponseEntity
+			.status(MypageCode.MEMBER_INFO_RETRIEVE_SUCCESS.getHttpStatus())
+			.body(resultDto);
+
+	}
+
+	// TODO : 사용자 정보 수정
+	@PatchMapping()
+	public void userInfoModify(
+		@AuthenticationPrincipal UserPrincipal userPrincipal
+	) {
+
+	}
+
+	// TODO : 키워드 관련 추가 예정
+
+	@GetMapping("/history/auction")
+	public ResponseEntity<ResultDto<Page<MypageAuctionListResponse>>> myAuctionList(
+		@AuthenticationPrincipal UserPrincipal userPrincipal,
+		@RequestParam(value = "auctionStatus", required = false) AuctionStatus auctionStatus,
+		@RequestParam(value = "sort", defaultValue = "createdAt") String sort,
+		@RequestParam(value = "direction", defaultValue = "DESC") String direction,
+		@PageableDefault(page = 0, size = 10)
+		Pageable pageable
+	) {
+
+		log.info("myAuctionList started");
+		log.debug("myAuctionList auctionStatus : {} , sort : {} , direction : {}", auctionStatus, sort, direction);
+
+		Long memberId = userPrincipal.getId();
+
+		Sort.Direction sortDirection = direction.equalsIgnoreCase("ASC") ? Sort.Direction.ASC : Sort.Direction.DESC;
+		pageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by(sortDirection, sort));
+
+		Page<MypageAuctionListResponse> auctionListResponses = mypageService
+			.findMyAuctionList(memberId, auctionStatus, pageable);
+
+		ResultDto<Page<MypageAuctionListResponse>> resultDto = ResultDto.res(
+			MypageCode.MY_AUCTION_LIST_SUCCESS.getStatusCode(),
+			MypageCode.MY_AUCTION_LIST_SUCCESS.getResultMsg(),
+			auctionListResponses
+		);
+
+		log.info("myAuctionList finished");
+		log.debug("myAuctionList resultDto : {}", resultDto);
+
+		return ResponseEntity
+			.status(MypageCode.MY_AUCTION_LIST_SUCCESS.getHttpStatus())
+			.body(resultDto);
+
+	}
+
+	@GetMapping("/history/bid")
+	public ResponseEntity<ResultDto<Page<MypageBidListResponse>>> myBidList(
+		@AuthenticationPrincipal UserPrincipal userPrincipal,
+		@RequestParam(value = "sort", defaultValue = "createdAt") String sort,
+		@RequestParam(value = "direction", defaultValue = "DESC") String direction,
+		@PageableDefault(page = 0, size = 10)
+		Pageable pageable
+	) {
+
+		log.info("myBidList started");
+		log.debug("myBidList sort : {} , direction : {} , pageable : {} ", sort, direction, pageable);
+
+		Long memberId = userPrincipal.getId();
+
+		Sort.Direction sortDirection = direction.equalsIgnoreCase("ASC") ? Sort.Direction.ASC : Sort.Direction.DESC;
+		pageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by(sortDirection, sort));
+
+		Page<MypageBidListResponse> auctionListResponses = mypageService.findMyBidList(memberId, pageable);
+
+		ResultDto<Page<MypageBidListResponse>> resultDto = ResultDto.res(
+			MypageCode.MY_BIDDING_LIST_SUCCESS.getStatusCode(),
+			MypageCode.MY_BIDDING_LIST_SUCCESS.getResultMsg(),
+			auctionListResponses
+		);
+
+		log.info("myBidList finished");
+		log.debug("myBidList resultDto : {} ", resultDto);
+
+		return ResponseEntity
+			.status(MypageCode.MY_BIDDING_LIST_SUCCESS.getHttpStatus())
+			.body(resultDto);
+
+	}
+}
---
+++ omocha-domain/src/main/java/org/auction/domain/bid/infrastructure/custom/BidRepositoryImpl.java
@@ -0,0 +1,53 @@
+package org.auction.domain.bid.infrastructure.custom;
+
+import static org.auction.domain.auction.domain.entity.QAuctionEntity.*;
+import static org.auction.domain.bid.entity.QBidEntity.*;
+import static org.auction.domain.image.domain.entity.QImageEntity.*;
+
+import java.util.List;
+
+import org.auction.domain.bid.entity.BidEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.support.PageableExecutionUtils;
+
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+
+public class BidRepositoryImpl implements BidRepositoryCustom {
+
+	private final JPAQueryFactory queryFactory;
+
+	public BidRepositoryImpl(EntityManager em) {
+		this.queryFactory = new JPAQueryFactory(em);
+	}
+
+	@Override
+	public Page<BidEntity> searchMyBidList(Long memberId, Pageable pageable) {
+
+		JPAQuery<BidEntity> query = queryFactory
+			.selectFrom(bidEntity)
+			.leftJoin(bidEntity.auctionEntity, auctionEntity)
+			.leftJoin(auctionEntity.images, imageEntity)
+			.where(bidEntity.memberEntity.memberId.eq(memberId));
+
+		for (Sort.Order o : pageable.getSort()) {
+			PathBuilder<?> pathBuilder = new PathBuilder<>(bidEntity.getType(), bidEntity.getMetadata());
+			query.orderBy(
+				new OrderSpecifier(o.isAscending() ? Order.ASC : Order.DESC, pathBuilder.get(o.getProperty())));
+		}
+
+		// 페이징 적용
+		List<BidEntity> questions = query.offset(pageable.getOffset()).limit(pageable.getPageSize()).fetch();
+
+		JPAQuery<Long> countQuery = queryFactory.select(bidEntity.count()).from(bidEntity);
+
+		return PageableExecutionUtils.getPage(questions, pageable, countQuery::fetchOne);
+	}
+}
