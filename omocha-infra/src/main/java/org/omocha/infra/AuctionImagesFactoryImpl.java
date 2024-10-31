66e07a1eff01b166ef49de4a455adbd96cf97f29
limbaba1120 <kwlim1120ss@gmail.com>
2024-10-31 11:40:27 +0900

    feat: imageprovider to domain layer

--- omocha-infra/src/main/java/org/omocha/infra/AuctionImagesFactoryImpl.java
+++ omocha-infra/src/main/java/org/omocha/infra/AuctionImagesFactoryImpl.java
@@ -8,6 +8,7 @@ import org.omocha.domain.auction.AuctionCommand;
 import org.omocha.domain.auction.AuctionImagesFactory;
 import org.omocha.domain.image.Image;
 import org.omocha.domain.image.ImageCommand;
+import org.omocha.domain.image.ImageProvider;
 import org.omocha.domain.image.ImageStore;
 import org.springframework.stereotype.Component;
 
