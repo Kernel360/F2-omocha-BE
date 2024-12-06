92c8106824087df3c5290f9c436402c92b09350b
limbaba1120 <kwlim1120ss@gmail.com>
2024-12-06 16:37:22 +0900

    feat: 입찰 시 redis lock 적용

---
+++ omocha-infra/src/main/java/org/omocha/infra/common/redis/CustomSpringELParser.java
@@ -0,0 +1,20 @@
+package org.omocha.infra.common.redis;
+
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+public class CustomSpringELParser {
+	private CustomSpringELParser() {
+	}
+
+	public static Object getDynamicValue(String[] parameterNames, Object[] args, String key) {
+		SpelExpressionParser parser = new SpelExpressionParser();
+		StandardEvaluationContext context = new StandardEvaluationContext();
+
+		for (int i = 0; i < parameterNames.length; i++) {
+			context.setVariable(parameterNames[i], args[i]);
+		}
+
+		return parser.parseExpression(key).getValue(context, Object.class);
+	}
+}
---
+++ omocha-infra/src/main/java/org/omocha/infra/common/redis/DistributedLockAop.java
@@ -0,0 +1,71 @@
+package org.omocha.infra.common.redis;
+
+import java.lang.reflect.Method;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.omocha.domain.common.annotation.DistributedLock;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Order(1)
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class DistributedLockAop {
+
+	private static final String REDISSON_LOCK_PREFIX = "redis-lock-";
+
+	private final RedissonClient redissonClient;
+	private final AopForTransaction aopForTransaction;
+
+	@Around("@annotation(org.omocha.domain.common.annotation.DistributedLock)")
+	public Object lock(final ProceedingJoinPoint joinPoint) throws Throwable {
+		MethodSignature signature = (MethodSignature)joinPoint.getSignature();
+		Method method = signature.getMethod();
+		DistributedLock distributedLock = method.getAnnotation(DistributedLock.class);
+
+		String key = REDISSON_LOCK_PREFIX +
+			CustomSpringELParser.getDynamicValue(
+				signature.getParameterNames(),
+				joinPoint.getArgs(),
+				distributedLock.key()
+			);
+		log.info("lock on [method:{}] [key:{}]", method, key);
+
+		RLock rLock = redissonClient.getLock(key);
+		String lockName = rLock.getName();
+		try {
+			boolean available =
+				rLock.tryLock(
+					distributedLock.waitTime(),
+					distributedLock.leaseTime(),
+					distributedLock.timeUnit()
+				);
+
+			if (!available) {
+				throw new RuntimeException("lock not available");
+			}
+
+			return aopForTransaction.proceed(joinPoint);
+		} catch (InterruptedException e) {
+			throw new RuntimeException("lock interrupted", e);
+		} finally {
+			try {
+				rLock.unlock();
+				log.info("unlock on [Lock:{}] [key:{}]", lockName);
+			} catch (IllegalMonitorStateException e) {
+				log.info("Redisson Lock Already Unlocked");
+			}
+		}
+
+	}
+}
