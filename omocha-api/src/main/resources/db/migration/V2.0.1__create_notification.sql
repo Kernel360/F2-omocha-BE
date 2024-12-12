4ccd56c3d6fcd2dfd777364d5be34f1fc47e997f
Haedeok <shd2768@naver.com>
2024-12-12 18:57:56 +0900

    feat: flyway 수정

--- omocha-api/src/main/resources/db/migration/V2.0.1__create_notification.sql
+++ omocha-api/src/main/resources/db/migration/V2.0.1__create_notification.sql
@@ -1,10 +1,12 @@
-CREATE TABLE notification (
-    notification_id BIGINT NOT NULL AUTO_INCREMENT,
-    member_id BIGINT,
-    event_name VARCHAR(255) NOT NULL,
+CREATE TABLE notification
+(
+    notification_id   BIGSERIAL PRIMARY KEY,
+    member_id         BIGINT,
+    event_name        VARCHAR(255) NOT NULL,
     notification_code VARCHAR(255) NOT NULL,
-    data TEXT NOT NULL,
-    read BOOLEAN NOT NULL,
-    PRIMARY KEY (notification_id),
+    data              TEXT         NOT NULL,
+    read              BOOLEAN      NOT NULL,
+    created_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_member FOREIGN KEY (member_id) REFERENCES member (member_id)
 );
